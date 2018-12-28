e32c1e53098bd28f5abf65d1d921aec9b26fc12f
mstroh <m.stroh@ymail.com>
2018-12-28 10:48:35 +0100

    change url

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@ rsync -v ../Blog/config.toml config.toml
 rsync -rv --delete ../Blog/content/* content
 rsync -rv --delete ../Blog/static/* static
 hugo
-chromium-browser "http://127.0.0.1:1313/Blog/" &
+chromium-browser "http://127.0.0.1:1313/" &
 hugo -D server
